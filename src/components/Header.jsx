72d58b594310019f506d26b5ea12c55518da5eb0
Richard Rodriguez-Van Dusen <richardRVD88@gmail.com>
2023-02-20 13:21:59 -0600

    Wireframe layout, css, html frontend work.

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Header = styled.h1`
+height: 100px;
+width: 45%;
+display: flex;
+border: 2px solid black;
+align-items: center;
+justify-content: center;
+font-size: 20pt;
+text-align: center;
+`;
+
+export default Header;
