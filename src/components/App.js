b6949239aa3151f7d77ce52d12d88f3f52641aa6
Richard Rodriguez-Van Dusen <richardRVD88@gmail.com>
2023-02-20 13:13:24 -0600

    basic design and components based off of wireframe. Used for starting point.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import Game from '../views/Game.js'
 
-import Home from './Home'
+import Home from '../views/Home'
 
 const App = () => (
   <div>
     <Route exact path='/' component={Home} />
+    <Route path='/game' component={Game} />
   </div>
 )
 
---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+background-color: grey;
+color: black;
+width: 190px;
+height: 75px;
+font-size: 1.5em;
+border: 2px solid black;
+border-radius: 5%;
+cursor: pointer;
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: center;
+`;
+export default Button;
