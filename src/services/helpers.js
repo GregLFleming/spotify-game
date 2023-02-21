71be934658ea3d39907377404a657298a7a826df
Gregory Fleming <flemdog13@hotmail.com>
2023-02-21 11:11:55 -0800

    removed comments

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -23,26 +23,3 @@ export const getRandomInt = (maxValue) => {
   maxValue = parseInt(maxValue);
   return Math.floor(Math.random() * max);
 }
-
-// export const timer = {
-//   // useEffect(() => {timer.clearTimer(timer.getDeadTime())}, []);
-
-//   start: function(timeOut, setTimeRemaining){
-//     const total = Date.parse(e) - Date.parse(new Date());
-//     const seconds = Math.floor((total / 1000) % 60);
-//     if (seconds >= 0) {
-//       setTimeRemaining(seconds)
-//     }
-//   },
-
-//   reset: function(timeLimit, setTimeRemaining, Ref){
-//     let timeOut = new Date();
-//     timeOut.setSeconds(timeOut.getSeconds() + timeLimit);
-//     setTimeRemaining(timeLimit);
-    
-//     if (Ref.current) clearInterval(Ref.current);
-//     const id = setInterval(() => {
-//       this.start(timeOut, setTimeRemaining);
-//     }, 1000)
-//     Ref.current = id;
-//   }
